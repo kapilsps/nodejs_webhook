70b0f2584f34bf2ef0573b0dd1fb87a43aec7f6b
kapilsps <kapil_kumar@softprodigy.com>
2021-05-23 19:47:56 +0530

    basic crud done

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const connectFlash = require('connect-flash');
+const session = require('express-session');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -19,16 +21,37 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+/**
+ * session
+ */
+app.use(session({
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: true,
+}));
+
+app.use(connectFlash());
+
+/** routes  */
+app.use((req, res, next) => {
+  res.locals.successMsg = req.flash('success');
+  res.locals.errorMsg = req.flash('fail');
+  res.locals.infoMsg = req.flash('info');
+  res.locals.warningMsg = req.flash('warning');
+  res.locals.error = req.flash('error');
+  res.locals.title = 'test';
+  next();
+});
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,92 @@
 var express = require('express');
 var router = express.Router();
+const { User } = require('../models');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+  User.findAll()
+  .then(result => {
+    res.render('list-user', { title: 'Express', data: result });
+  })
+  .catch(err => {
+    req.flash('fail', err.message);
+    res.render('list-user', { title: 'Express'});
+  });
+});
+
+router.get('/create', (req, res, next) => {
+  res.render('create-user', { title: 'Express' });
+});
+
+router.post('/create', (req, res, next) => {
+    User.create({
+      firstName: req.body.first_name,
+      lastName: req.body.last_name,
+      email: req.body.email
+    })
+    .then(result => {
+      if (result !== null) {
+        req.flash('success', 'User Registered successfully.')
+        return res.redirect('/users/');
+      }
+      req.flash('fail', 'Something went wrong please try again')
+      return res.redirect('/users/create');
+    })
+    .catch(err => {
+      req.flash('fail', err.message)
+      return res.redirect('/users/create');
+    });
+});
+
+
+router.get('/update-user/:id', (req, res, next) => {
+    User.findByPk(req.params.id)
+    .then(result => {
+        res.render('update',{
+          title: 'Express',
+          data: result
+        });
+    })
+    .catch(err => {
+        req.flash('fail', err.message);
+        res.redirect('/users');
+    });
+});
+
+router.post('/update/:id', (req, res, next) => {
+  User.update({
+    firstName: req.body.first_name,
+    lastName: req.body.last_name,
+    email: req.body.email
+  },{
+    where:{
+      id:req.params.id
+    }
+  })
+  .then((result) => {
+    req.flash('success','User updated successfully');
+    res.redirect('/users');
+  })
+  .catch(err => {
+    req.flash('fail', err.message);
+    res.redirect('/users');
+  });
+});
+
+router.get('/delete-user/:id', (req, res, next) => {
+  User.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(result => {
+    req.flash('success', 'user deleted successfully');
+    res.redirect('/users');
+  })
+  .catch(err => {
+    req.flash('fail', err.message);
+    res.redirect('/users');
+  });
 });
 
 module.exports = router;
